09c258660581b682de92da76658a3fe563f58f18
SFA24SCM37M <smadithati@hawk.iit.edu>
2024-10-23 00:52:57 -0500

    update

--- s.java
+++ s.java
@@ -6,5 +6,4 @@ SFA24SCM04D
 SFA24SCM91L
 SFA24SCM69P
 SFA24SCM02G
-SFA24SCM01S    
-SFA24SCM37M                                                                                                                                                                                                                                                          
+SFA24SCM01S                                                                                                                                                                                                                                                          
